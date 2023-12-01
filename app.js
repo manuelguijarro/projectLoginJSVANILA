c276d2f71a4b74dc81b986f17558492101bcb4aa
manuelguijarro <manu2506.1994@gmail.com>
2023-12-01 19:48:12 +0100

    regex for password input

--- app.js
+++ app.js
@@ -3,43 +3,54 @@ const username = document.getElementById("username");
 const password = document.getElementById("password");
 const btn = document.getElementById("btn");
 
-//Evento para el boton, hacer click y validar datos con base de datos.
-btn.addEventListener("click", (e) => {
-  //Para que no se envien los datos, se evita el evento y asi podriamos
-  //hacer un update o validar los datos
-  e.preventDefault();
-  if (username.value === "" || password.value === "") {
-    alert("Please enter username and password");
-  } else {
-    alert("Login successful");
-    window.location.href = "home.html";
-    username.value = "";
-    password.value = "";
-  }});
-//Con los eventos blur verificamos si una vez que el usuario hace click fuera
-// se lanza un aviso.
-username.addEventListener("blur", () => {
-  validateData();
-});
+const validatePasswordRegex = () => {
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  const passwordValue = password.value;
+  if (!passwordRegex.test(passwordValue)) return false;
+  return true;
+};
 
-password.addEventListener("blur", () => {
-  validateData();
-});
+const areFieldsEmpty = () => {
+  return username.value === "" || password.value === "";
+};
 
-// Add event listener for focus on button
-btn.addEventListener("focus", () => {
-  validateData();
-});
-
-// Function to validate data and redirect to home page with alert message
-function validateData() {
-  if (username.value === "" || password.value === "") {
-    alert("Please enter username and password");
-  } else {
-    alert("Login successful");
-    window.location.href = "home.html";
-    username.value = "";
-    password.value = "";
+const clearFields = () => {
+  username.value = "";
+  password.value = "";
+};
+const loginSuccessful = () => {
+  if (areFieldsEmpty()) {
+    alert("Complete all fields");
+    return;
   }
+  if(!validatePasswordRegex()){
+    alert("Password must contain at least 8 characters with at least one uppercase letter, one number, and one special character");
+    return;
+  }
+  
+  alert("Login successful");
+  clearFields();
+  window.location.href = "home.html";
+};
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginSuccessful();
+});
+
+// Function to validate username input
+const validateUsername = () =>  {
+  if (!username.value) alert("Please enter username");
 }
+
+const validatePassword = () => {
+  if (!password.value) alert("Please enter password");
+}
+
+username.addEventListener("blur", validateUsername);
+password.addEventListener("blur", validatePassword);
+
+
+
+
 
